2f20a8bc4a72fd9688f464d2232aae8149a495c9
thabolinake <thabolinake@gmail.com>
2025-03-14 19:56:08 +0200

    RTA - prep - created test case - create loan object

---
+++ src/test/java/linax/org/radixtechassessment/controller/LoanControllerTest.java
@@ -0,0 +1,38 @@
+package linax.org.radixtechassessment.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import linax.org.radixtechassessment.model.Loan;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class LoanControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Test
+    void testCreateLoan() throws Exception {
+
+        Loan loan = new Loan();
+        loan.setAmount(100.0);
+        loan.setTerm(12);
+
+        mockMvc.perform(
+                MockMvcRequestBuilders.post("/api/loans")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(loan))
+        )
+                .andExpect(MockMvcResultMatchers.status().isCreated());
+    }
+}
