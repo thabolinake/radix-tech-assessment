b36528052ae78b454f1129f2d1f3539b53ae9370
thabolinake <thabolinake@gmail.com>
2025-03-14 13:34:51 +0200

    RTA - initial push

---
+++ src/main/java/linax/org/radixtechassessment/RadixTechAssessmentApplication.java
@@ -0,0 +1,13 @@
+package linax.org.radixtechassessment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RadixTechAssessmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RadixTechAssessmentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/linax/org/radixtechassessment/RadixTechAssessmentApplicationTests.java
@@ -0,0 +1,13 @@
+package linax.org.radixtechassessment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RadixTechAssessmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
